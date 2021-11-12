38372ef08664290b62aa6744e542d0f53f7aa308
richard.sun <richard.sun@ucloud.cn>
2021-11-12 19:02:21 +0800

    #richard.sun: fix

--- pkg/fw/fw_test.go
+++ pkg/fw/fw_test.go
@@ -48,7 +48,7 @@ func Test_write(t *testing.T) {
 	}
 	for n, p := range writeTestData {
 		f := func(t *testing.T) {
-			var ctx, cancel = context.WithTimeout(context.Background(), 3*time.Second)
+			var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 			defer cancel()
 			err = s.Write(ctx, p.name, p.version, p.rules)
 			if err != nil {
