e3bd9ff8b8544b9a7b9d1eecfd8f291958f2b7ce
richard.sun <richard.sun@ucloud.cn>
2021-11-14 09:39:54 +0800

    #richard.sun: 定义map

---
+++ bpf/fw.bpf.c
@@ -0,0 +1,22 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+//定义数据Map和用户态通讯
+struct bpf_map_def SEC("maps") iptables = {
+	.type    	 = BPF_MAP_TYPE_HASH,
+    .key_size	 = 0x10,
+    .value_size	 = 0x20,
+    .max_entries = 0x10,
+    .map_flags	 = BPF_F_NO_PREALLOC,
+};
+
+SEC("xdp/fw")
+int xpd_handle_fw(struct xdp_md *ctx) {
+    int rc = XDP_PASS;
+        return rc;
+}
+
+char _license []SEC("license") = "GPL";
+
+
+//clang -g -Wall -O2 -c -target bpf -D__TARGET_ARCH_x86 fw.bpf.c -I/usr/include/x86_64-linux-gnu/ -o fw.bpf.o 
