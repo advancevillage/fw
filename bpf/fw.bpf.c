11007029d4c5c3eca0381fc0b7dd30a04e704990
richard.sun <richard.sun@ucloud.cn>
2021-12-08 22:00:33 +0800

    #richard.sun: bpf

--- bpf/fw.bpf.c
+++ bpf/fw.bpf.c
@@ -6,8 +6,10 @@
 #include <linux/udp.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
-#include <string.h>
 
+#define keySize     0x10
+#define valueSize   0x04
+#define maxEntry    (0x01 << 16) 
 
 //定义元数据
 //map name:         metadata
@@ -17,70 +19,109 @@
 struct bpf_map_def SEC("maps") metadata = {
     .type        = BPF_MAP_TYPE_HASH,
     .key_size	 = 0x10,
-    .value_size	 = 0x04,
+    .value_size	 = 0x08,
     .max_entries = 0x20,
     .map_flags	 = BPF_F_NO_PREALLOC,
 };
 
 struct bpf_map_def SEC("maps") ipv4_proto = {
     .type        = BPF_MAP_TYPE_LPM_TRIE,
-    .key_size	 = 0x10,
-    .value_size	 = 0x04,
-    .max_entries = 10000,
+    .key_size	 = keySize,
+    .value_size	 = valueSize,
+    .max_entries = maxEntry,
     .map_flags	 = BPF_F_NO_PREALLOC,
 };
 
 struct bpf_map_def SEC("maps") ipv4_action = {
     .type        = BPF_MAP_TYPE_LPM_TRIE,
-    .key_size	 = 0x10,
-    .value_size	 = 0x04,
-    .max_entries = 10000,
+    .key_size	 = keySize,
+    .value_size	 = valueSize,
+    .max_entries = maxEntry,
     .map_flags	 = BPF_F_NO_PREALLOC,
 };
 
 struct bpf_map_def SEC("maps") ipv4_nw_dst = {
     .type        = BPF_MAP_TYPE_LPM_TRIE,
-    .key_size	 = 0x10,
-    .value_size	 = 0x04,
-    .max_entries = 10000,
+    .key_size	 = keySize,
+    .value_size	 = valueSize,
+    .max_entries = maxEntry,
     .map_flags	 = BPF_F_NO_PREALLOC,
 };
 
 struct bpf_map_def SEC("maps") ipv4_nw_src = {
     .type        = BPF_MAP_TYPE_LPM_TRIE,
-    .key_size	 = 0x10,
-    .value_size	 = 0x04,
-    .max_entries = 10000,
+    .key_size	 = keySize,
+    .value_size	 = valueSize,
+    .max_entries = maxEntry,
     .map_flags	 = BPF_F_NO_PREALLOC,
 };
 
 struct bpf_map_def SEC("maps") ipv4_tp_dst = {
     .type        = BPF_MAP_TYPE_LPM_TRIE,
-    .key_size	 = 0x10,
-    .value_size	 = 0x04,
-    .max_entries = 10000,
+    .key_size	 = keySize,
+    .value_size	 = valueSize,
+    .max_entries = maxEntry,
     .map_flags	 = BPF_F_NO_PREALLOC,
 };
 
 struct bpf_map_def SEC("maps") ipv4_tp_dst = {
     .type        = BPF_MAP_TYPE_LPM_TRIE,
-    .key_size	 = 0x10,
-    .value_size	 = 0x04,
-    .max_entries = 10000,
+    .key_size	 = keySize,
+    .value_size	 = valueSize,
+    .max_entries = maxEntry,
     .map_flags	 = BPF_F_NO_PREALLOC,
 };
 
 static __inline __be64 *query_meta_fw_zone() {
     char name[0x10];
-    memset(name, 0, 0x10); 
-    strcpy(name, "fw.zone");
+    name[0x00] = 'f';
+    name[0x01] = 'w';
+    name[0x02] = '.';
+    name[0x03] = 'z';
+    name[0x04] = 'o';
+    name[0x05] = 'n';
+    name[0x06] = 'e';
+    name[0x07] = 0x00;
+    name[0x08] = 0x00;
+    name[0x09] = 0x00;
+    name[0x0a] = 0x00;
+    name[0x0b] = 0x00;
+    name[0x0c] = 0x00;
+    name[0x0d] = 0x00;
+    name[0x0e] = 0x00;
+    name[0x0f] = 0x00;
     __be64 *zone = (__be64*)bpf_map_lookup_elem(&metadata, name);
     if (!zone) {
         return -1;
     }
     return (*zone);
 }
-//TODO query bitmat
+
+static __inline unsigned char* query_fw_proto_bits(__be64 zone, __u8 proto) {
+    unsigned char kk[keySize];
+    kk[0x00] = 0x60;
+    kk[0x01] = 0x00;
+    kk[0x02] = 0x00;
+    kk[0x03] = 0x00;
+    kk[0x04] = __u8(zone >> 56) & 0xff;
+    kk[0x05] = __u8(zone >> 48) & 0xff;
+    kk[0x06] = __u8(zone >> 40) & 0xff;
+    kk[0x07] = __u8(zone >> 32) & 0xff;
+    kk[0x08] = __u8(zone >> 24) & 0xff;
+    kk[0x09] = __u8(zone >> 16) & 0xff;
+    kk[0x0a] = __u8(zone >> 8)  & 0xff;
+    kk[0x0b] = __u8(zone)       & 0xff;
+    kk[0x0c] = 0x00;
+    kk[0x0d] = 0x00;
+    kk[0x0e] = 0x00;
+    kk[0x0f] = proto;
+    
+    unsigned char *bits = (unsigned char*)bpf_map_lookup_elem(&ipv4_proto, kk);
+    if (!bits) {
+        return NULL;
+    }
+    return bits;
+}
 
 static __inline int security_strategy(__u8 proto, __be32 src_ip, __be16 src_port, __be32 dst_ip, __be16 dst_port) {
     int rc = XDP_DROP;  //默认拒绝
@@ -89,8 +130,11 @@ static __inline int security_strategy(__u8 proto, __be32 src_ip, __be16 src_port
     if (zone < 0) {
         goto leave;
     }
-    //void *proto_bit = query_fw_proto_bitmap(proto_fd, proto);
-    //bpf_printk("proto_bit=%x\n", *((unsigned char*)proto_bit));
+    unsigned char *proto_bits = query_fw_proto_bits(zone, proto);
+    if (!proto_bits) {
+        bpf_printk("proto_bits=%x\n", *proto_bits);
+        goto leave;
+    }
 
     rc = XDP_PASS;
 
