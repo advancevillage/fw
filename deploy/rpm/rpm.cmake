8ddb4b6ee266c72bed7279f2dd04991506cbda03
richard.sun <richard.sun@ucloud.cn>
2021-12-20 16:34:45 +0800

    #richard.sun: 软件包命名规则 名称-版本-修正版-类型 software-1.2.3-1.tar.gz

--- deploy/rpm/rpm.cmake
+++ deploy/rpm/rpm.cmake
@@ -1,4 +1,5 @@
 #https://cmake.org/cmake/help/v3.3/module/CPackRPM.html
+#https://rpm-packaging-guide.github.io
 set(CPACK_GENERATOR                         "RPM")
 set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE      "${CMAKE_CURRENT_SOURCE_DIR}/deploy/rpm/install")
 set(CPACK_RPM_PRE_UNINSTALL_SCRIPT_FILE     "${CMAKE_CURRENT_SOURCE_DIR}/deploy/rpm/uninstall")
